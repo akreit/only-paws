e8d7d9a5029703ce2839ef586145ce40699d7877
akreit <a.kreitschmann@gmail.com>
2026-07-05 14:57:39 +0200

    address additional lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt(
   // Your custom configs here
   {
+    ignores: ['.github/**'],
     rules: {
       'vue/multi-word-component-names': 'off',
       'vue/no-multiple-template-root': 'off',
